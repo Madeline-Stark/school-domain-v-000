c955fc27d2e939c880e9c6bc75bd4e9c720b811e
Madeline Stark <madeline.stark1@gmail.com>
2018-04-08 22:43:25 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+require 'pry'
+
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    unless roster.include?(grade)
+      roster[grade] = []
+    end
+    roster[grade] << student_name
+  end
+
+  def grade(year)
+    roster[year]
+  end
+
+  def sort
+  roster.collect do |grade, student_name|
+    "#{roster[grade]}=>"
+      student_name.sort do |a, b|
+        a <=> b
+      end
+  end
+  end
+
+end
