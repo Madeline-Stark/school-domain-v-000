d9398240b8a76a4d50490a7d157182e0d7686250
Madeline Stark <madeline.stark1@gmail.com>
2018-04-08 23:38:00 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -24,8 +24,7 @@ def sort
   hash = {}
   roster.each do |grade, student_names|
     new_student_array << student_names.sort
-    binding.pry
-  hash[grade] << "grade => new_student_array"
+  hash[grade] << new_student_array
   end
   hash
 end
